bca1a0d1aa643c71dbb85599824bcb9260c27a14
Shymaa Ashraf <shymaaashraf876@gmail.com>
2021-05-24 03:34:07 +0200

    Add Business Layer in BL Branch

---
+++ BL/Bases/BaseAppService.cs
@@ -0,0 +1,25 @@
+﻿using BL.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Bases
+{
+    public class BaseAppService : IDisposable
+    {
+        protected IUnitOfWork TheUnitOfWork { get; set; }
+        protected readonly IMapper Mapper; //MapperConfig.Mapper;
+        public BaseAppService(IUnitOfWork theUnitOfWork, IMapper mapper)
+        {
+            TheUnitOfWork = theUnitOfWork;
+            Mapper = mapper;
+        }
+        public void Dispose()
+        {
+            TheUnitOfWork.Dispose();
+        }
+    }
+}
+
---
+++ BL/Bases/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using BL.interfaces;
+using Microsoft.AspNet.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Bases
+{
+   public class UnitOfWork : IUnitOfWork
+    {
+        private DbContext EC_DbContext { get; set; }
+        private UserManager<User> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+        public UnitOfWork(ApplicationDBContext EC_DbContext, UserManager<ApplicationUserIdentity> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            this._userManager = userManager;
+            this._roleManager = roleManager;
+            this.EC_DbContext = EC_DbContext;
+            // Avoid load navigation properties
+            //EC_DbContext.Configuration.LazyLoadingEnabled = false;
+        }
+        public int Commit()
+        {
+            return EC_DbContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            EC_DbContext.Dispose();
+        }
+    }
+}
+

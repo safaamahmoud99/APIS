66df167462b7102d5655660d78834cef84b090e8
Eman-AbdElZaher <e.m.abdelzaher@gmail.com>
2021-05-25 14:52:37 +0200

    Add to Dtos and Repository

---
+++ BL/DTOs/CategoryViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.DTOs
+{
+   public class CategoryViewModel
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public int MainCategoryID { get; set; }
+        public string MainCategoryName { get; set; }
+    }
+}
---
+++ BL/DTOs/SubCategoryViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.DTOs
+{
+    public class SubCategoryViewModel
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
